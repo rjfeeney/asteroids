e904098408f58add1d378cdd2aa9234889527af5
ThePrimeagen <rileyfeeney7@gmail.com>
2024-11-14 19:55:12 -0500

    Chapter 2 Lesson 2

---
+++ main.py
@@ -0,0 +1,26 @@
+import pygame
+from constants import *
+
+def main():
+        pygame.init()
+        print("Starting asteroids!")
+        print(f"Screen width: {SCREEN_WIDTH}")
+        print(f"Screen height: {SCREEN_HEIGHT}")
+        
+        # Set a display variable before creating the window
+        if not pygame.display.get_init():
+            pygame.display.init()
+            
+        screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+        pygame.display.set_caption("Asteroids")
+        
+        while True:
+                for event in pygame.event.get():
+                        if event.type == pygame.QUIT:
+                                pygame.quit()
+                                return
+                screen.fill("black")
+                pygame.display.flip()
+
+if __name__ == "__main__":
+    main()
